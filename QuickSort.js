8a9495053c0b677848a95a5e8be119e08b295a8f
Rasha Zayed <zayed.rasha@outlook.com>
2020-09-27 21:39:47 -0700

    edit swap function

--- QuickSort.js
+++ QuickSort.js
@@ -5,11 +5,11 @@ function quickSorting(arr, low, high) {
     quickSorting(arr, p + 1, high);
   }
 }
-function swap(num1, num2) {
+function swap(arr, num1, num2) {
   var temp;
-  temp = num1;
-  num1 = num2;
-  num2 = temp;
+  temp = arr[num1];
+  arr[num1] = arr[num2];
+  arr[num2] = temp;
 }
 
 function partition(arr, low, high) {
@@ -19,9 +19,12 @@ function partition(arr, low, high) {
   for (j = low; j < high; j++) {
     if (arr[j] < pivot) {
       i++;
-      swap(arr[j], arr[i]);
+      swap(arr, i, j);
     }
   }
-  swap(arr[i + 1], arr[high]);
+  swap(arr, i + 1, high);
   return i + 1;
 }
+var arr = [5, 6, 3, 8, 2, 8, 4, 9];
+quickSorting(arr, 0, 7);
+console.log(arr);
